bd050b371bb95fe4fb1e77c85d8446fbf439b7ca
Ankit raj <ankitrajxd@outlook.com>
2024-02-16 21:15:28 +0530

    fixed animation

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -11,14 +11,14 @@ const ResumePage = () => {
       <Link href={"../"}>
         <MdOutlineKeyboardBackspace className="my-[1rem] mt-[2rem]" size={30} />
       </Link>
-      <div className="max-w-[400px]  overflow-hidden">
-        <Image alt="myresume" className="rounded-md fill-muted-foreground" src={ankitresume} />
+      <AnimatedDiv ClassName="max-w-[400px]  overflow-hidden">
+        <Image alt="myresume" className="rounded-md" src={ankitresume} />
         <a href={"../../ankitresume.pdf"} download>
           <Button variant={"outline"} className="my-3">
             Download
           </Button>
         </a>
-      </div>
+      </AnimatedDiv>
     </AnimatedDiv>
   );
 };
