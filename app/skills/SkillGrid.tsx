bb6997b3356bb4b3117182d9d63d5fb305e611cd
Ankit <ankitrajxd@gmail.com>
2024-02-18 21:20:39 +0530

    removed unused imports

--- app/skills/SkillGrid.tsx
+++ app/skills/SkillGrid.tsx
@@ -1,11 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-"use client";
-import { Tooltip } from "react-tooltip";
 
-import styles from "./skills.module.css";
 import { skills } from "../data/data";
-import TooltipComponent from "../components/TooltipComponent";
 import Skill from "./Skill";
 
 const Skills = () => {
