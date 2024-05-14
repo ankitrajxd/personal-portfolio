49d454b118d894347ceba27bcee8014e85d6b483
Ankit <ankitrajxd@gmail.com>
2024-05-14 21:47:41 +0530

    fixed component

--- app/components/AOSWrapper.tsx
+++ app/components/AOSWrapper.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, ReactNode } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-const AOSWrapper = ({ children }) => {
+interface AOSWrapperProps {
+  children: ReactNode;
+}
+
+const AOSWrapper: React.FC<AOSWrapperProps> = ({ children }) => {
   useEffect(() => {
     AOS.init({
       duration: 1000, // Set the duration of animations in milliseconds
