6eae6f4b22fd8529829c2566713d422817ff48c9
Ankit <ankitrajxd@gmail.com>
2024-05-14 14:49:03 +0530

    fixed emoji

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="h-auto my-[5rem] md:my-0 md:h-[70vh] pr-3 flex items-center justify-center ">
         <div className="mx-[5rem] md:mx-[3rem] lg:mx-[12rem] w-full relative flex items-center justify-start sm:justify-center">
           <h1 className="text-5xl font-bold md:text-7xl xl:text-[5rem] text-left sm:text-center leading-tight md:leading-tight ">
-            Hi, 🙋 I&apos;m{" "}
+            Hi👋, I&apos;m{" "}
             <span className=" decoration-2 underline-offset-[5px] decoration-[#74E291]">
               {/* <span className="bg-clip-text font-protest font-normal  text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 ">
                 Ankit,
