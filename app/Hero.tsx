fba4d6573c0baba5c25d87a4ab6cf2744cc35eff
Ankit <ankitrajxd@gmail.com>
2024-06-06 22:58:01 +0530

    fixed padding

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -5,26 +5,9 @@ const Hero = () => {
     <>
       <div
         data-aos="fade"
-        className="h-auto mb-[2.5rem] md:my-0 md:h-[70vh] flex items-center justify-center "
+        className="h-auto mb-[1rem] md:my-0 md:h-[70vh] flex items-center justify-center "
       >
         <div className="mx-[5rem] md:mx-[3rem] p-10 lg:mx-[12rem] w-full relative flex items-center justify-start sm:justify-center">
-          {/* <div className="absolute -left-1 top-0">
-          <span className="w-3 h-[1.2px] bg-white bg-opacity-70 absolute"></span>
-          <span className="w-3 h-[1.2px] rotate-90 bg-white  bg-opacity-70 absolute"></span>
-        </div>
-
-        <div className="absolute right-1 top-0">
-          <span className="w-3 h-[1.2px] bg-white bg-opacity-70 absolute"></span>
-          <span className="w-3 h-[1.2px] rotate-90 bg-white  bg-opacity-70 absolute"></span>
-        </div>
-        <div className="absolute -left-1 bottom-0">
-          <span className="w-3 h-[1.2px] bg-white bg-opacity-70 absolute"></span>
-          <span className="w-3 h-[1.2px] rotate-90 bg-white  bg-opacity-70 absolute"></span>
-        </div>
-        <div className="absolute bottom-0 right-1">
-          <span className="w-3 h-[1.2px] bg-white bg-opacity-70 absolute"></span>
-          <span className="w-3 h-[1.2px] rotate-90 bg-white  bg-opacity-70 absolute"></span>
-        </div> */}
           <h1 className="text-5xl font-bold md:text-7xl xl:text-[5rem] text-left sm:text-center leading-tight md:leading-tight ">
             Hi, I&apos;m{" "}
             <span className="bg-clip-text font-protest font-normal   text-transparent bg-gradient-to-r from-[#FDBA66] to-[#FD6D71] ">
--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="overflow-hidden">
-      <div id="top" className="px-[1rem] xl:px-[9rem] mt-[3rem] md:mt-0">
+      <div id="top" className="px-[1rem] xl:px-[9rem] mt-[1.5rem] md:mt-0">
         <Hero />
       </div>
-      <div id="skills" className="my-[3rem] mb-[6.25rem] sm:mt-[5rem] relative">
+      <div id="skills" className="my-[4rem] mb-[6.25rem] sm:mt-[5rem] relative">
         <InfiniteMarquee
           direction="left"
           className="-rotate-6 sm:rotate-0 w-[101%]"
