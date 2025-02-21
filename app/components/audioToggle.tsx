f2b51b1f5ed89a30993aa63cdc3469250071d2b7
Ankit <ankitrajxd@gmail.com>
2025-02-21 13:55:48 +0530

    Add AudioToggle component for audio playback control and state management

---
+++ app/components/audioToggle.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Image from "next/image";
+import { useState, useRef, useEffect } from "react";
+
+const AudioToggle = () => {
+  const [toggle, setToggle] = useState(false);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    // Initialize Audio only on client-side
+    audioRef.current = new Audio("/audio.mp3");
+
+    const storedTime = localStorage.getItem("audioTime");
+    if (storedTime && audioRef.current) {
+      audioRef.current.currentTime = parseFloat(storedTime);
+    }
+
+    const handleTimeUpdate = () => {
+      if (audioRef.current) {
+        localStorage.setItem(
+          "audioTime",
+          audioRef.current.currentTime.toString()
+        );
+      }
+    };
+
+    audioRef.current.addEventListener("timeupdate", handleTimeUpdate);
+    window.addEventListener("beforeunload", handleTimeUpdate);
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.removeEventListener("timeupdate", handleTimeUpdate);
+      }
+      window.removeEventListener("beforeunload", handleTimeUpdate);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      if (toggle) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [toggle]);
+
+  return (
+    <button
+      onClick={() => setToggle((prev) => !prev)}
+      className="cursor-pointer hover:opacity-100 w-5 rounded-full outline p-1 outline-1 outline-zinc-600"
+    >
+      {toggle ? (
+        <Image
+          src={"/audio.png"}
+          alt="music-on"
+          className="invert opacity-100"
+          width={100}
+          height={110}
+        />
+      ) : (
+        <Image
+          src={"/mute.png"}
+          alt="music-off"
+          className="invert opacity-50"
+          width={100}
+          height={110}
+        />
+      )}
+    </button>
+  );
+};
+
+export default AudioToggle;
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import AudioToggle from "./audioToggle";
 
 interface Props {
   className?: string;
@@ -25,32 +26,35 @@ const Header = ({ className }: Props) => {
         />
       </Link>
 
-      <div
-        className={
-          "flex gap-1 items-center bg-nav p-1 rounded-full border-[1px] border-zinc-800 text-zinc-500 text-sm"
-        }
-      >
-        <span
-          className={`${
-            pathName === "/" && "btn-active"
-          } py-1.5 px-3 rounded-full hover:text-zinc-300`}
+      <div className="flex items-center gap-2">
+        <AudioToggle />
+        <div
+          className={
+            "flex gap-1 items-center bg-nav p-1 rounded-full border-[1px] border-zinc-800 text-zinc-500 text-sm"
+          }
         >
-          <Link href={"/"}>Home</Link>
-        </span>
-        <span
-          className={`${
-            pathName === "/about" ? "btn-active" : ""
-          }    py-1.5 px-3 rounded-full hover:text-zinc-300`}
-        >
-          <Link href={"/about"}>About</Link>
-        </span>
-        <span
-          className={`${
-            pathName === "/projects" && "btn-active"
-          } py-1.5 px-3 rounded-full hover:text-zinc-300`}
-        >
-          <Link href={"/projects"}>Work</Link>
-        </span>
+          <span
+            className={`${
+              pathName === "/" && "btn-active"
+            } py-1.5 px-3 rounded-full hover:text-zinc-300`}
+          >
+            <Link href={"/"}>Home</Link>
+          </span>
+          <span
+            className={`${
+              pathName === "/about" ? "btn-active" : ""
+            }    py-1.5 px-3 rounded-full hover:text-zinc-300`}
+          >
+            <Link href={"/about"}>About</Link>
+          </span>
+          <span
+            className={`${
+              pathName === "/projects" && "btn-active"
+            } py-1.5 px-3 rounded-full hover:text-zinc-300`}
+          >
+            <Link href={"/projects"}>Work</Link>
+          </span>
+        </div>
       </div>
     </nav>
   );
